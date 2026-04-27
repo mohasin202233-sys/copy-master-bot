a9737c98da09fd1e78c559eb9afd72d770808e92
mohasin202233-sys <mohasin202233@gmail.com>
2026-04-28 02:20:38 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-import telebot
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 from telebot import types
 import sqlite3
 from datetime import datetime, timedelta
